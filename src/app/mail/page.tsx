15248743c71514f91d5a5d00a67b96c59f7ca46a
Udit Shukla <uditshukla731@gmail.com>
2025-06-16 15:29:09 +0530

    added suspense

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,10 +1,13 @@
 import { MailLayout } from '@/components/MailLayout';
 import { EnrichedEmailList } from '@/components/EnrichedEmailList';
+import { Suspense } from 'react';
 
 export default function MailPage() {
   return (
     <MailLayout>
-      <EnrichedEmailList />
+      <Suspense fallback={<div>Loading...</div>}>
+        <EnrichedEmailList />
+      </Suspense>
     </MailLayout>
   );
 } 
